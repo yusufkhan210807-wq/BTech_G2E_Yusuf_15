ddd9206032e4c9db6976b9f2e06711b85ce98e03
yusufkhan210807-wq <yusufkhan210807@gmail.com>
2026-02-10 14:59:24 +0530

    Add files via upload

---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,12 @@
+
+int main{ 
+    float m1,m2,m3,m4,m5;
+    float sum,percentage;
+    Printf ( "Enter the marks of 5 subjects:\n");
+    Scanf("%f%f%f%f%f", & m1,&m2,&m3,&m4,&m5);
+    Sum = m1+m2+m3+m4+m5;
+    Percentage= (Sum/500)*100;
+    Printf("Total Marks=%.2f\n,"sum)
+    Printf("Total Percentage marks=%.2f%f%f\n",Percentage); 
+    return 0;
+}
