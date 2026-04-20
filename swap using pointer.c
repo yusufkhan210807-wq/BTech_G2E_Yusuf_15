29b598b4839bd2185ac7fe0406108a6f3aa0e917
yusufkhan210807-wq <yusufkhan210807@gmail.com>
2026-04-20 22:41:46 +0530

    Create swap using pointer.c

---
+++ swap using pointer.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+
+void swap(int *a, int *b) {
+    int temp;
+    temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main() {
+    int x, y;
+
+    
+    printf("Enter two numbers: ");
+    scanf("%d %d", &x, &y);
+
+    
+    swap(&x, &y);
+
+    
+    printf("After swapping:\n");
+    printf("x = %d, y = %d\n", x, y);
+
+    return 0;
+}
