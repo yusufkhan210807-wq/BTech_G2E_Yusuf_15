e3e64198a2f19b27d28a6a05e75dbeaf9e5c4282
yusufkhan210807-wq <yusufkhan210807@gmail.com>
2026-04-20 22:38:37 +0530

    Create reverse of no.c

---
+++ reverse of no.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int n, reverse = 0, rem;
+
+    
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    
+    while(n != 0) {
+        rem = n % 10;                 
+        reverse = reverse * 10 + rem; 
+        n = n / 10;                  
+    }
+
+    // Output
+    printf("Reversed number = %d\n", reverse);
+
+    return 0;
+}
