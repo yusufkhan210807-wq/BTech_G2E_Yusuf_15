2d28c19175e44156aab7c5ca07a21f874fd93107
yusufkhan210807-wq <yusufkhan210807@gmail.com>
2026-04-20 22:44:55 +0530

    Create voting.c

---
+++ voting.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int age;
+    printf("Enter your age: ");
+    scanf("%d", &age);
+
+    if (age >= 18) {
+        printf("You are eligible to vote.\n");
+    } else {
+        printf("You are not eligible to vote.\n");
+        printf("You can vote after %d years.\n", 18 - age);
+    }
+    return 0;
+}
